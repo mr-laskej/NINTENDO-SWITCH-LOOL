8ae5aa0bc650c73d3d8cd539499e6a2d6de6d1df
matyas.lasky.020 <matyas.lasky.020@PC2017-09>
2022-02-07 13:43:33 +0100

    Přidejte soubory projektu.

---
+++ NINTENDO.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int a = 2, b = 3;
+	printf("jakou operaci? \n");
+	printf("1 - soucet\n");
+	printf("2 - rozdil\n");
+	printf("3 - soucin\n");
+	printf("4 - podil\n");
+	int volba = 0;
+	float vysledek;
+	scanf("%d", &volba);
+	switch (volba) {
+	case 1:
+		vysledek = a + b;
+		break;
+	case 2:
+		vysledek = a - b;
+		break;
+	case 3:
+		vysledek = a * b;
+		break;
+	case 4: 
+		vysledek = (float) a / b;
+		break;
+	default:
+		printf("neplatna volba");
+		}
+	printf("vysledek: %f", vysledek);
+}
+
